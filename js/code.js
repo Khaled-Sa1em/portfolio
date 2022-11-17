da5dc5d94ec21208df435726da2685e682055a50
Khaled-Sa1em <khaled.abdulrahman.salem1@gmail.com>
2022-11-18 01:39:53 +0200

    ADD nav bar and give a some styles ADD js to maintain switching between sections ADD active class in _commons.scss

---
+++ js/code.js
@@ -0,0 +1,16 @@
+// make area of sections
+const sections = document.querySelectorAll(".section");
+// use bubbling
+// or propagation
+// make event on nave itself
+// then get the target id
+// then subtract section id from it
+// then remove active class from all section
+// then give to the section with id we abstract from target id
+const nav = document.addEventListener("click", (e) => {
+  console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
+  sections.forEach((section) => section.classList.remove("active"));
+  document
+    .getElementById(e.target.id.substring(0, e.target.id.indexOf("-")))
+    .classList.add("active");
+});

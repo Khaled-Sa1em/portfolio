0185e6fad470cf1ac8e3db338af91a52600d40cd
Khaled-Sa1em <khaled.abdulrahman.salem1@gmail.com>
2022-11-18 02:52:16 +0200

    ADD some transitions on nav bar

--- js/code.js
+++ js/code.js
@@ -1,5 +1,6 @@
 // make array of sections
 const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".nav--link");
 // use bubbling
 // or propagation
 // make event on nave itself
@@ -9,8 +10,26 @@ const sections = document.querySelectorAll(".section");
 // then give to the section with id we abstract from target id
 const nav = document.addEventListener("click", (e) => {
   console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
-  sections.forEach((section) => section.classList.remove("active"));
-  document
-    .getElementById(e.target.id.substring(0, e.target.id.indexOf("-")))
-    .classList.add("active");
+  console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
+  if (e.target.id.substring(0, e.target.id.indexOf("-")) !== "nav") {
+    console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
+    sections.forEach((section) => section.classList.remove("active"));
+    document
+      .getElementById(e.target.id.substring(0, e.target.id.indexOf("-")))
+      .classList.add("active");
+
+    navLinks.forEach((link) => {
+      link.classList.remove("display--link");
+    });
+  } else {
+    if (e.target.classList.contains('display--link')) {
+      navLinks.forEach((link) => {
+        link.classList.remove("display--link");
+      });
+    } else {
+      navLinks.forEach((link) => {
+        link.classList.add("display--link");
+      });
+    }
+  }
 });

f1eeee22fa2c7e9ce8771cd933a54ba29ccbfd77
Khaled-Sa1em <khaled.abdulrahman.salem1@gmail.com>
2022-11-18 03:03:30 +0200

    ADD active--link and ADD code to maintain it with comments

--- js/code.js
+++ js/code.js
@@ -9,24 +9,34 @@ const navLinks = document.querySelectorAll(".nav--link");
 // then remove active class from all section
 // then give to the section with id we abstract from target id
 const nav = document.addEventListener("click", (e) => {
-  console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
-  console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
   if (e.target.id.substring(0, e.target.id.indexOf("-")) !== "nav") {
-    console.log(e.target.id.substring(0, e.target.id.indexOf("-")));
+    // remove active class from all sections first
     sections.forEach((section) => section.classList.remove("active"));
+
+    // remove active class from all links first
+    navLinks.forEach((link) => {
+      link.classList.remove("active--link");
+    });
+    // then add active link to selected section
     document
       .getElementById(e.target.id.substring(0, e.target.id.indexOf("-")))
       .classList.add("active");
 
+    // add active--link to make clicked link display: none
+    e.target.classList.add("active--link");
+
+    // hide all nav links
     navLinks.forEach((link) => {
       link.classList.remove("display--link");
     });
   } else {
-    if (e.target.classList.contains('display--link')) {
+    if (e.target.classList.contains("display--link")) {
+      // hide all links if click on nav link
       navLinks.forEach((link) => {
         link.classList.remove("display--link");
       });
     } else {
+      // show all nav links
       navLinks.forEach((link) => {
         link.classList.add("display--link");
       });

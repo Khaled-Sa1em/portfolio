62cde499559ef9798af2b9ddb9a0a38b03724b4d
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-29 07:15:03 +0200

    ADD real links @ social-icons

--- js/data/SocialNavItems.js
+++ js/data/SocialNavItems.js
@@ -2,10 +2,40 @@
 // * array of js objects
 
 export const SocialNavItems = [
-    { itemName: "linkedin", itemIconClass: "icon-linkedin", itemTitle: "linkedin" },
-    { itemName: "github", itemIconClass: "icon-github-circled", itemTitle: "github" },
-    { itemName: "twitter", itemIconClass: "icon-twitter", itemTitle: "twitter" },
-    { itemName: "facebook", itemIconClass: "icon-facebook", itemTitle: "facebook" },
-    { itemName: "instagram", itemIconClass: "icon-instagram", itemTitle: "instagram" },
-    { itemName: "whatsapp", itemIconClass: "icon-whatsapp", itemTitle: "whatsapp" },
+    {
+        itemName: "linkedin",
+        itemIconClass: "icon-linkedin",
+        itemTitle: "linkedin",
+        itemLink: "https://www.linkedin.com/feed/"
+    },
+    {
+        itemName: "github",
+        itemIconClass: "icon-github-circled",
+        itemTitle: "github",
+        itemLink: "https://github.com/"
+    },
+    {
+        itemName: "twitter",
+        itemIconClass: "icon-twitter",
+        itemTitle: "twitter",
+        itemLink: "https://twitter.com/"
+    },
+    {
+        itemName: "facebook",
+        itemIconClass: "icon-facebook",
+        itemTitle: "facebook",
+        itemLink: "https://www.facebook.com/"
+    },
+    {
+        itemName: "instagram",
+        itemIconClass: "icon-instagram",
+        itemTitle: "instagram",
+        itemLink: "https://www.instagram.com/"
+    },
+    {
+        itemName: "whatsapp",
+        itemIconClass: "icon-whatsapp",
+        itemTitle: "whatsapp",
+        itemLink: "https://web.whatsapp.com/"
+    },
 ]
--- js/navbar.js
+++ js/navbar.js
@@ -10,9 +10,13 @@ export const setNavbar = (elementToAppendIn, itemsInList = [], navType) => {
 
         if (navType === 'primary') {
 
-            const oneRow = ` <li class="navbar__list--item navbar__primary__list--item">
-            <a href="#" class="navbar__list--item-link" data-title="${el.itemTitle}">
-            <i class="demo-icon ${el.itemIconClass}"></i></a></li>`
+            const oneRow =
+                `<li class="navbar__list--item navbar__primary__list--item">
+                    <a href="#" class="navbar__list--item-link" data-title="${el.itemTitle}">
+                        <i class="demo-icon ${el.itemIconClass}"></i>
+                    </a>
+                </li>`;
+
             elementToAppendIn.insertAdjacentHTML('beforeend', oneRow);
 
         } else {
@@ -21,25 +25,33 @@ export const setNavbar = (elementToAppendIn, itemsInList = [], navType) => {
             const radius = 65;
 
             let th = index * theta;
+
             if (el.id === 'root') {
                 return;
             }
+
             let x = radius * Math.sin(th * Math.PI / 180)
             let y = radius * Math.cos(th * Math.PI / 180)
 
-            const oneRow = ` <li class="navbar__list--item " style = "--translate:translate(${x}px,${-y}px); --transition-delay: .${index}s "  >
-            <a href="#" target='_blank' class="navbar__list--item-link " id ='${el.itemTitle}' data-title="${el.itemTitle}">
-            <i class="demo-icon ${el.itemIconClass}"></i></a></li>`
+            const oneRow =
+                `<li class="navbar__list--item " style="--translate:translate(${x}px,${-y}px); --transition-delay: 0.${index}s">
+                    <a href="#" target='_blank' class="navbar__list--item-link " id='${el.itemTitle}' data-title="${el.itemTitle}">
+                        <i class="demo-icon ${el.itemIconClass}"></i>
+                    </a>
+                </li>`;
 
 
             elementToAppendIn.insertAdjacentHTML('beforeend', oneRow);
 
             const link = document.querySelector(`#${el.itemTitle}`);
+
             link.addEventListener('click', function (e) {
                 e.preventDefault();
+
                 setTimeout(function () {
-                    window.open('https://www.google.com/');
-                }, "2000");
+                    link.href = el.itemLink;
+                    window.open(link.href);
+                }, "1800");
             });
         }
     });
@@ -53,6 +65,7 @@ export const openNavList = () => {
     //  ! check this ==>>>>  (navbar__primary__list) instead of (navbar__list) 
     document.querySelectorAll(".navbar__primary__list li")?.forEach((el) => el.classList.toggle("active"));
 };
+
 // !* copy i will develop it 
 export const openSocialNavList = () => {
     document.querySelector('#navbar-social').classList.toggle('active');
